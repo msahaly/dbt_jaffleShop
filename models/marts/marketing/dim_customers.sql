2b12ad0eaff2590fcafb4f6611ac5f69f33ed44b
Mohammed Anwar Sahaly <m_sahaly@hotmail.com>
2026-02-27 09:49:37 +0000

    Added haffleShop source YAML

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -4,11 +4,17 @@ with customers as (
 
 ),
 
+-- orders as (
+
+--     SELECT * FROM {{ ref('stg_jaffelShop__orders') }}
+
+-- ),
+
+
 orders as (
-
-    SELECT * FROM {{ ref('stg_jaffelShop__orders') }}
-
+    select * from {{ ref ('fct_orders')}}
 ),
+
 
 customer_orders as (
 
@@ -17,13 +23,16 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
 
     group by 1
 
 ),
+
+
 
 
 final as (
@@ -34,10 +43,10 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
 
     from customers
-
     left join customer_orders using (customer_id)
 
 )
