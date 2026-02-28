b22c31815700740020522a57fad55cfb61061c77
Mohammed Anwar Sahaly <m_sahaly@hotmail.com>
2026-02-28 18:09:31 +0000

    Added singular test to stg_stripe__payemnts

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -11,8 +11,8 @@ renamed as (
     select
         id as payment_id,
         orderid as order_id,
-        paymentmethod,
-        status as payment_method,
+        paymentmethod as payment_method,
+        status as payment_status,
         -- amount is stored in cents, convert it to dollars
         amount / 100 as amount,
         
